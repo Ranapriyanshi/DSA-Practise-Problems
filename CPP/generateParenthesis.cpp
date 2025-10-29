783f0d0391c6903db12773ebad3c6e2b4e18fcff
Ranapriyanshi <112609820+Ranapriyanshi@users.noreply.github.com>
2025-10-29 17:33:49 +0530

    Generated Parenthesis Problem

---
+++ CPP/generateParenthesis.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+
+// int randomArray(int &arr[]){
+
+
+// }
+
+int main(){
+    // vector<int>arr;
+    int arr[5];
+    arr = [3,4,5,2,1];
+    // randomArray(arr);
+    for(int i=1;i<=5;i++){
+        count<<rand();
+    }
+}
